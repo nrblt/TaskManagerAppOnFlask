c8bd358bd3a24869e5a91ee4109b7302037e1758
macbook <nurboturbont@mail.ru>
2021-10-18 19:19:49 +0600

    1st Commit

---
+++ app.py
@@ -0,0 +1,82 @@
+# from urllib import request
+
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+# from config import config
+import sys
+# from flask.ext.sqlalchemy import SQLAlchemy
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql://root:root@127.0.0.1:8889/FlaskDB'
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db=SQLAlchemy(app)
+# a
+class Todo(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    content=db.Column(db.String(200),nullable=False)
+    completed=db.Column(db.Integer,default=0)
+    date_created=db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+
+# class Example(db.Model):
+#     __tablename__ = 'example'
+#     id = db.Column('id', db.Integer, primary_key=True)
+#     data = db.Column('data', db.Unicode)
+#     def __init__(self,id,data):
+#         self.id = id
+#         self.data = data
+# #
+# new_ex = Example(5,'fifth entry')
+# db.session.add(new_ex)
+# db.session.commit()
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method=='POST':
+        # return "HEllo"
+        taskContent=request.form['content']
+        newTask=Todo(content=taskContent)
+
+        try :
+            db.session.add(newTask)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There is issue'
+    else:
+        tasks=Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html',tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    deleteTask=Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(deleteTask)
+        db.session. commit()
+        return redirect('/')
+    except:
+        return deleteTask.content
+
+@app.route('/update/<int:id>',methods=['GET','POST'])
+def  update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method=='POST':
+        task.content=request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There is the issue'
+    else:
+        return render_template('update.html',task=task)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
